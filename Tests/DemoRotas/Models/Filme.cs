cf2f41b44a1b754e89c21e5539a69c82fd855a27
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-02-04 21:16:49 -0300

    Model: Início

---
+++ Tests/DemoRotas/Models/Filme.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoRotas.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public DateTime DataLancamento { get; set; } = DateTime.Now;
+        public string Genero { get; set; } = string.Empty;
+        public decimal Valor { get; set; } = decimal.Zero;
+        public int Avaliacao { get; set; }
+        public bool Ativo { get; set; } = true;
+    }
+}
