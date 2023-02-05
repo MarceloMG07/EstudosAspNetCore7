3c917b38edaa5a866a31c1e69c66f2d660aa6f33
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-02-05 15:41:02 -0300

    View: ViewComponent

---
+++ Tests/DemoViews/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoViews.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent() { 
+            ItensCarrinho = 7;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
