130fab2aabe07214f74bded55be65d141f1641ae
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-02-05 18:35:15 -0300

    View: Formularios

---
+++ Tests/DemoViews/Controllers/FilmeController.cs
@@ -0,0 +1,89 @@
+﻿using DemoViews.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoViews.Controllers
+{
+    public class FilmeController : Controller
+    {
+        // GET: FilmeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FilmeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FilmeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Filme filme)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                }
+                //return RedirectToAction(nameof(Index));
+                return View(filme);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Tests/DemoViews/Models/Filme.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoViews.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres.")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato incorreto.")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; } = DateTime.Now;
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Genero em formato inválido.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres.")]
+        public string Genero { get; set; } = string.Empty;
+
+        [Range(1, 1000, ErrorMessage = "Valor de {1} a {2}")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; } = decimal.Zero;
+
+        [
+            Required(ErrorMessage = "O campo {0} é obrigatório."), 
+            RegularExpression(
+                @"^[0-5]*$", 
+                ErrorMessage = "O valor deve ser {1} de 0 a 5."
+            ), 
+            Display(Name = "Avaliação")
+        ]
+        public int Avaliacao { get; set; }
+        public bool Ativo { get; set; } = true;
+    }
+}
